16d80b20302fcfa847cba788cffd1983b306fb68
JoshKoster <62957883+JoshKoster@users.noreply.github.com>
2020-05-18 14:21:53 -0600

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,20 @@
 const rollButton = document.querySelector("#roll-button")
 const dieRolls = []
 
-rollButton.addEventListener("click", function() {
+// rollButton.addEventListener("click", function() {
     
-})
+// })
 
 const sides = ["One" , "Two" , "Three" , "Four" , "Five" , "Six"];
 const roll = () => sides[Math.floor(Math.random() * 6)]
 
-// const rolls = (n) => {
-//     const result = [];
-//     for (let i = 0; i < n; i++) {
-//         result.push(roll());
-//     }
-//     return result;
-// };
+const rolls = (n) => {
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(roll());
+    }
+    return result;
+};
 
 const count = (rolls) => {
     const result = new Map();
@@ -24,3 +24,29 @@ const count = (rolls) => {
             1 + (result.get(roll) || 0));
     }
     return result;
+}
+
+let diceRollDisplay = document.querySelector('#dice-roll-display');
+diceRollDisplay.addEventListener("click", function(){
+    console.log("dice-roll-display");
+})
+
+
+let roll = document.querySelector('#roll');
+roll.addEventListener("click", function(){
+    console.log('roll');
+})
+
+var dieRolls = new Array();
+
+
+let showAll = document.querySelector('#show-All');
+showAll.addEventListener("click", function(){
+    console.log("#show-all");
+})
+
+
+let numberOfDiceRoll = document.querySelector('#number-of-dice-roll');
+numberOfDiceRoll.addEventListener("click", function(){
+    console.log("number-of-dice-roll");
+})
